1707ed598c1ae506ed05a17bb44bd1c5701fd4cc
elgasing23 <abinaufal901@gmail.com>
2025-09-03 16:50:29 +0800

    Complete tutorial 1: Django MVT implementation

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+def show_main(request):
+    context = {
+        'npm' : '2406361694',
+        'name': 'M Naufal Zhafran Rabiul Batara',
+        'class': 'PBP F'
+    }
+
+    return render(request, "main.html", context)
+# Create your views here.
